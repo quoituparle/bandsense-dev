86aded73889b679185eb4097cef4fb38d1322fa9
quoituparle <daiyue.han@outlook.com>
2025-09-03 23:40:37 +0800

    add email verification

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Login from './auth/login'
+import Registration from './auth/register'
+import Verification from './auth/verify-email'
+import Main from './client/main'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/verify-email' element={<Verification />} />
+      </Routes>
+    </div>
   )
 }
 
