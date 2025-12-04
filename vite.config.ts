a6564567a54bd5813db1bb3c3866c14f18840642
quoituparle <daiyue.han@outlook.com>
2025-12-04 20:12:45 +0800

    add vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://api.flyflypig.online',
         changeOrigin: true, 
         rewrite: (path) => path.replace(/^\/api/, ''), 
       },
